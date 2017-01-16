65f2b341395c17d6aa916e2f415a9d5a64e49211
blizzarac <stolz.alexander@gmail.com>
2017-01-16 22:58:28 +0100

    database work

--- scripts/createdb.sql
+++ scripts/createdb.sql
@@ -1,10 +1,35 @@
-DROP TABLE IF EXISTS Snp;
+DROP TABLE IF EXISTS Nutrient;
+DROP TABLE IF EXISTS Food;
+DROP TABLE IF EXISTS Log;
 
 
-CREATE TABLE SNP
+CREATE TABLE Nutrient
 (
-  rsid VARCHAR(255),
-  chromosome VARCHAR(5),
-  position INTEGER,
-  genotype VARCHAR(50)
+  id INTEGER,
+  name VARCHAR(255),
+  dose
+);
+
+CREATE TABLE Food
+(
+  id INTEGER,
+  Calories	INTEGER,
+  Carbs INTEGER,	
+  Fat INTEGER,	
+  Protein	INTEGER,
+  Cholest INTEGER,	
+  Sodium	INTEGER,
+  Sugars	INTEGER,
+  Fiber INTEGER
+);
+
+CREATE TABLE NutrientInFood(
+  id INTEGER,
+  nutrient Nutrient,
+  food Food
+);
+
+CREATE TABLE Log(
+  id INTEGER,
+  date Date,
 );
